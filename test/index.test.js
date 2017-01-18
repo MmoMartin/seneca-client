b48197ae4335b951485c285a70efc850e0ecd6b7
FengYuHe <heyufeng8080@gmail.com>
2017-01-18 16:39:28 +0800

    add travis

---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+const seneca = require('seneca');
+const expect = require('chai').expect;
+
+const senecaClient = require('./../index');
+const port = '8082';
+const client = new senecaClient({port: port, host: '127.0.0.1'});
+
+function test () {
+  this.add('role:test, cmd:run', (msg, done) => {
+    done(null, {code: 200, data: msg.data.test});
+  })
+}
+
+describe('service client', () => {
+  before(done => {
+    seneca({log: {level: 'info+'}})
+      .use(test)
+      .listen({type: 'http', port, pin: 'role:test'})
+      .ready(() => {
+        done();
+      });
+  });
+
+  it('send method for callback', done => {
+    client.send({module: 'test', cmd: 'run', data: {test: 'test success'}}, function (err, result) {
+      expect(err).to.be.equal(null);
+      expect(result.code).to.be.equal(200);
+      expect(result.data).to.be.equal('test success');
+      done();
+    });
+  });
+
+  it('send method for promise', done => {
+    client.send({module: 'test', cmd: 'run', data: {test: 'test success'}}).then((result) => {
+      expect(result.code).to.be.equal(200);
+      expect(result.data).to.be.equal('test success');
+      done();
+    });
+  });
+});
